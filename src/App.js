07b657c9e8f78f6474cc85c7ff958828b790af7f
Pushmitha Pandarinath <pushmithapandarinath@gmail.com>
2026-02-01 14:21:25 +0530

    Add playto_frontend

--- src/App.js
+++ src/App.js
@@ -1,23 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+import Feed from "./components/Feed";
+import Leaderboard from "./components/Leaderboard";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{ padding: 20 }}>
+      <h1>Community Feed</h1>
+      <Leaderboard />
+      <Feed />
     </div>
   );
 }
